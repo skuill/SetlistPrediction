1e5205f2bc200e9e3bf0a10584fcbf12c00dd0fd
skuill <sativkv@gmail.com>
2017-12-08 01:11:17 +0300

    Add helpful information in file "References.txt"

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+
 import argparse
 import musicbrainzngs
 from musicbrainzngs import WebServiceError
@@ -9,27 +9,25 @@ musicbrainzngs.set_useragent(
         "https://github.com/skuill/SetlistPrediction"
 )
 
-if __name__ == '__main__':    
+if (__name__ == '__main__'):    
     parser = argparse.ArgumentParser(prog="SetlistPrediction")
     requiredNamed = parser.add_argument_group('required named arguments')
     requiredNamed.add_argument('-u', '--username', type=str, help="Specify username", action="store", required=True)
     requiredNamed.add_argument('-p', '--password', type=str, help="Specify password", action="store", required=True)
     
-    args = parser.parse_args(['-h'])
+    args = parser.parse_args()
     
     if args.username and args.password:
         print ("User: {}. Password: {}".format(args.username, args.password))
         
         musicbrainzngs.auth(args.username, args.password)
     
-        artist_id = "c5c2ea1c-4bde-4f4d-bd0b-47b200bf99d6"
         try:
-            result = musicbrainzngs.get_artist_by_id(artist_id)
+            result = musicbrainzngs.search_artists(artist="parkway drive", type="group")
         except WebServiceError as exc:
             print("Something went wrong with the request: %s" % exc)
         else:
-            artist = result["artist"]
-            print("name:\t\t%s" % artist["name"])
-            print("sort name:\t%s" % artist["sort-name"])
+            for artist in result['artist-list']:
+                print(u"{id}: {name}".format(id=artist['id'], name=artist["name"]))
     else:
         parser.print_help()
