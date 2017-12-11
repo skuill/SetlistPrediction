c908b62193de5be7944679609c190d3ba796c0ec
skuill <sativkv@gmail.com>
2017-12-11 18:45:00 +0300

    1) Get tour events for artist from setlist.fm in pandas DataFrame object. 2) Parameter -sfmk for setlist.fm API key

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
 import sys
 import argparse
+from setlistfm import SetlistGetter
+import pandas as pd
 
 from musicbrainz import MusicbrainzSearcher
 
 if (__name__ == '__main__'):    
     parser = argparse.ArgumentParser(prog="SetlistPrediction")
     requiredNamed = parser.add_argument_group('required named arguments')
-    requiredNamed.add_argument('-u', '--username', type=str, help="Specify username", action="store", required=True)
-    requiredNamed.add_argument('-p', '--password', type=str, help="Specify password", action="store", required=True)
+    requiredNamed.add_argument('-u', '--username', 
+                               type=str, help="Specify username", 
+                               action="store", required=True)
+    requiredNamed.add_argument('-p', '--password', 
+                               type=str, help="Specify password", 
+                               action="store", required=True)
+    requiredNamed.add_argument('-sfmk', '--setlistfm_key', 
+                               type=str, help="Specify setlistfm API key", 
+                               action="store", required=True)
     
     args = parser.parse_args()
     
-    if args.username and args.password:
-        print ("User: {}. Password: {}".format(args.username, args.password))
+    if args.username and args.password and args.setlistfm_key:
+        print ("User: {}. Password: {}. Setlistfm API key: {}.".format(args.username, args.password, args.setlistfm_key))
     else:
         parser.print_help()   
         sys.exit("Good Bye!")     
     
     #search_artist = input("Prompt artist or group name: ")
     #search_artist = "Parkway drive"
-    search_artist = "iron maiden"
+    search_artist = "Parkway Drive"
     print ("Search for artist:", search_artist)
     if search_artist:
         musicbrainz_searcher = MusicbrainzSearcher(args.username, args.password)       
         interesting_artist = musicbrainz_searcher.get_musicbrainz_artist_info(search_artist)
         print(interesting_artist)
+        setlistGetter = SetlistGetter(args.setlistfm_key)
+        events = setlistGetter.get_artist_events(interesting_artist, 5)
+        print (events)
+        
             
             
         
--- setlistfm.py
+++ setlistfm.py
@@ -7,11 +7,10 @@ except:
 	import json
 
 import requests
-import csv
-import sys
+import pandas as pd
 
 class SetlistGetter:
-    columns = ['eventID',
+    _events_columns = ['eventID',
 	                'artist',
 	                'eventdate',
 	                'tourname',
@@ -29,3 +28,66 @@ class SetlistGetter:
     def __init__(self, api_key):
         self._api_key = api_key
     
+    def get_artist_events(self, artist, pages_count = 1):
+        # Call Setlist.fm API
+        url = 'https://api.setlist.fm/rest/1.0/artist/' + artist._mbid + '/setlists?p=1'
+        headers = {'Accept': 'application/json', 'x-api-key': self._api_key}
+        r = requests.get(url, headers=headers)	
+        # Get .json Data
+        data = r.json()
+        # Get total number of shows
+        totalshows = int(data['total'])
+        # Total Number of Pages needed to load
+        pages_exists = int(totalshows/20)
+        if (pages_exists < pages_count):
+            pages = pages_exists
+        else:
+            pages = pages_count
+        result_events_df = pd.DataFrame(columns = self._events_columns)
+        for page in range(1,pages):
+            url = 'https://api.setlist.fm/rest/1.0/artist/' + artist._mbid + '/setlists?p=' + str(page)
+            headers = {'Accept': 'application/json', 'x-api-key': self._api_key}
+            r = requests.get(url, headers=headers)	
+    		  # Get .json Data
+            data = r.json()
+            #print (data)
+    		  # Read .json file line per line
+            for line in data:
+                for i in range(len(data['setlist'])):
+                    tourName = ''
+                    tour = data['setlist'][i].get('tour')
+                    if tour is not None:
+                        tourName = tour['name']
+                    result_events_df = result_events_df.append(
+				                {
+				                # Event ID
+				                'eventID': str(data['setlist'][i]['id']),
+				                # Artist
+				                'artist': str(data['setlist'][i]['artist']['name']),
+				                # Eventdate
+				                'eventdate': str(data['setlist'][i]['eventDate']),
+				                # TourName
+				                'tourname': tourName,
+				                # Venue
+				                'venue': str(data['setlist'][i]['venue'].get('name')),
+				                # Venue ID
+				                'venue_id': str(data['setlist'][i]['venue'].get('id')),
+				                # City
+				                'city': str(data['setlist'][i]['venue']['city'].get('name')),
+				                # City ID
+				                'city_id': str(data['setlist'][i]['venue']['city'].get('id')),
+				                # City Latitude
+				                'city_lat': float(data['setlist'][i]['venue']['city']['coords'].get('lat')),
+				                # City Longitude
+				                'city_lon': float(data['setlist'][i]['venue']['city']['coords'].get('long')),
+				                # State
+				                'state': str(data['setlist'][i]['venue']['city'].get('state')),
+				                # State Code
+				                'state_id': str(data['setlist'][i]['venue']['city'].get('stateCode')),
+				                # Country
+				                'country': str(data['setlist'][i]['venue']['city']['country'].get('name')),
+				                # Country Code
+				                'country_id': str(data['setlist'][i]['venue']['city']['country'].get('code'))
+				                }, ignore_index=True
+				                )
+        return result_events_df
