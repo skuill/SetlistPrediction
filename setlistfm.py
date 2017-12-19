e97f96e23c7ce1e8a8f112fcf88f7cd2306f56bd
skuill <sativkv@gmail.com>
2017-12-19 18:31:26 +0300

    Add get two dataframes for events and setlists. Logic of parsing 'sets' moved to separate method.

--- setlistfm.py
+++ setlistfm.py
@@ -51,7 +51,9 @@ class SetlistGetter:
         # Total Number of Pages needed to load
         pages_count = math.ceil(events_count/20)
         result_events_df = pd.DataFrame(columns = self._events_columns)
+        result_setlist_df = pd.DataFrame(columns = self._set_columns)
         processed_events = 0
+        
         for page in range(pages_count):
             url = 'https://api.setlist.fm/rest/1.0/artist/' + artist._mbid + '/setlists?p=' + str(page+1)
             headers = {'Accept': 'application/json', 'x-api-key': self._api_key}
@@ -65,12 +67,17 @@ class SetlistGetter:
                     break
                 tourName = ''
                 tour = data['setlist'][i].get('tour')
+                event_id = str(data['setlist'][i]['id'])
+                if ('sets' in data['setlist'][i]):
+                    current_sets_df = self.parse_sets_dictionary(data['setlist'][i]['sets'])
+                    current_sets_df['event_id']=event_id
+                    result_setlist_df = pd.concat([result_setlist_df, current_sets_df], ignore_index=True)                
                 if tour is not None:
                     tourName = tour['name']
                 result_events_df = result_events_df.append(
                         {
                                 # Event ID
-                                'event_id': str(data['setlist'][i]['id']),
+                                'event_id': event_id,
                                 # Artist
                                 'artist': str(data['setlist'][i]['artist']['name']),
                                 # Eventdate
@@ -100,7 +107,26 @@ class SetlistGetter:
                         }, ignore_index=True)
                 processed_events += 1
         result_events_df['eventdate'] = pd.to_datetime(result_events_df['eventdate'], format='%d-%m-%Y')
-        return result_events_df.sort_values(by=['eventdate'], ascending=False)
+        return (result_events_df.sort_values(by=['eventdate'], ascending=False), result_setlist_df)
+    
+    def parse_sets_dictionary(self, sets_dictionary):
+        result_setlist_df = pd.DataFrame(columns = self._set_columns)        
+        song_num = 0
+        for i in range(len(sets_dictionary['set'])):
+            encore = 0
+            if 'encore' in sets_dictionary['set'][i]:
+                encore = 1
+            for j in range(len(sets_dictionary['set'][i]['song'])):
+                result_setlist_df = result_setlist_df.append( 
+                    {
+                        'song_num': song_num,
+                        'name': sets_dictionary['set'][i]['song'][j].get('name'),
+                        'encore': encore,
+                        'tape': sets_dictionary['set'][i]['song'][j].get('tape', False),
+                        'info': sets_dictionary['set'][i]['song'][j].get('info', ''),   
+                    }, ignore_index = True)
+                song_num+=1
+        return result_setlist_df
     
     def get_setlist_for_event(self, event_id):
         # Call Setlist.fm API
@@ -111,21 +137,7 @@ class SetlistGetter:
         data = r.json()
         result_setlist_df = pd.DataFrame(columns = self._set_columns)
         if ('sets' in data):
-            song_num = 0
-            for i in range(len(data['sets']['set'])):
-                encore = 0
-                if 'encore' in data['sets']['set'][i]:
-                    encore = 1
-                for j in range(len(data['sets']['set'][i]['song'])):
-                    result_setlist_df = result_setlist_df.append( 
-                        {
-                            'event_id': event_id,
-                            'song_num': song_num,
-                            'name': data['sets']['set'][i]['song'][j].get('name'),
-                            'encore': encore,
-                            'tape': data['sets']['set'][i]['song'][j].get('tape', False),
-                            'info': data['sets']['set'][i]['song'][j].get('info', ''),   
-                        }, ignore_index = True)
-                    song_num+=1
+            result_setlist_df = pd.concat([result_setlist_df, self.parse_sets_dictionary(data['sets'])], ignore_index=True)
+        result_setlist_df['event_id']=event_id
         return result_setlist_df
                 
